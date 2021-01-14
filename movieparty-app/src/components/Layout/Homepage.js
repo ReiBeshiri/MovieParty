1b84d6c37710470a6ed871a152c962683b4dff03
Andrea Vaienti <andrea.vaienti2@studio.unibo.it>
2021-01-14 18:55:05 +0100

    update homepage graphic

--- movieparty-app/src/components/Layout/Homepage.js
+++ movieparty-app/src/components/Layout/Homepage.js
@@ -4,13 +4,15 @@ import Navbar from "./Navbar";
 import "./Homepage.css";
 
 
-function Homepage(props) {
+function Homepage() {
         return (
             <div className = "backgroundImage">
-                <div class = "valign-wrapper fullscreen">
+                <Navbar/>
+                <div class = "prova">
                     <div class ="fullwidth center-align">
-                        <h1 class = "moviepartyTitle">MovieParty</h1>
-                        <br/>
+                        <img class="mediumSize responsive-img" src="img/logo.svg"/>
+                        <h1 class = "moviepartyTitle">A new way to watch movies together</h1>
+                        {/* <br/>
                         <Link
                             to="/register"
                             className="btn btn-large waves-effect waves-light hoverable red lighten-1 accent-3 registerButton"
@@ -22,98 +24,12 @@ function Homepage(props) {
                             className="btn btn-large waves-effect waves-light hoverable red lighten-1 accent-3 loginButton"
                         >
                             Log In
-                        </Link>
+                        </Link> */}
                     </div>
                 </div>
             </div>
-            /*<div class = "valign-wrapper fullscreen">
-                    <div class="row">
-                        <div class="col s1">1</div>
-                        <div class="col s1">2</div>
-                        <div class="col s1">3</div>
-                        <div class="col s1">4</div>
-                        <div class="col s1">5</div>
-                        <div class="col s1">6</div>
-                        <div class="col s1">7</div>
-                        <div class="col s1">8</div>
-                        <div class="col s1">9</div>
-                        <div class="col s1">10</div>
-                        <div class="col s1">11</div>
-                        <div class="col s1">12</div>
-                    </div>
-                    <br></br>
-                    <div className="row">
-                        <div class = "col s6">
-                            <Link
-                                to="/register"
-                                style={{
-                                width: "140px",
-                                borderRadius: "3px",
-                                letterSpacing: "1.5px"
-                                }}
-                                className="btn btn-large waves-effect waves-light hoverable blue accent-3"
-                            >
-                                Register
-                            </Link>
-                        </div>
-                        <div class = "col s6">
-                            <Link
-                                to="/login"
-                                style={{
-                                width: "140px",
-                                borderRadius: "3px",
-                                letterSpacing: "1.5px"
-                                }}
-                                className="btn btn-large btn-flat waves-effect white black-text"
-                            >
-                                Log In
-                            </Link>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div style={{ height: "75vh" }} className="container valign-wrapper">
-                    <div className="row">
-                    <div className="col s12 center-align">
-                        <h4>
-                            <b>Build</b> a login/auth app with the{" "}
-                            <span style={{ fontFamily: "monospace" }}>MERN</span> 
-                            stack from scratch
-                        </h4>
-                        <p className="flow-text grey-text text-darken-1">
-                        Create a (minimal) full-stack app with user authentication via
-                        passport and JWTs
-                        </p>
-                        <br/>
-                        <div className="col s6">
-                        <Link
-                            to="/register"
-                            style={{
-                            width: "140px",
-                            borderRadius: "3px",
-                            letterSpacing: "1.5px"
-                            }}
-                            className="btn btn-large waves-effect waves-light hoverable blue accent-3"
-                        >
-                            Register
-                        </Link>
-                        </div>
-                        <div className="col s6">
-                        <Link
-                            to="/login"
-                            style={{
-                            width: "140px",
-                            borderRadius: "3px",
-                            letterSpacing: "1.5px"
-                            }}
-                            className="btn btn-large btn-flat waves-effect white black-text"
-                        >
-                            Log In
-                        </Link>
-                        </div>
-                    </div>
-                    </div>
-                        </div>*/
+
+            
         );
 }
 
--- movieparty-app/src/components/Layout/Navbar.js
+++ movieparty-app/src/components/Layout/Navbar.js
@@ -1,21 +1,48 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
+import M from "materialize-css";
+import "./Navbar.css";
 
-function Navbar(props){
-        return (
-            <nav>
+function Navbar(){
+
+    useEffect(() => {
+        const  elems = document.querySelectorAll('.sidenav');
+        M.Sidenav.init(elems, {});
+    }, [])
+
+    return (
+        <div>
+            <nav class = "transparent z-depth-0">
                 <div class="nav-wrapper">
-                    <a href="#!" class="brand-logo">Logo</a>
-                    <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+                    {/* <img class="logo responsive-img" src="img/logo.svg"/> */}
+                    <a href="/" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
                     <ul class="right hide-on-med-and-down">
-                        <li><a href="sass.html">Sass</a></li>
-                        <li><a href="badges.html">Components</a></li>
-                        <li><a href="collapsible.html">Javascript</a></li>
-                        <li><a href="mobile.html">Mobile</a></li>
+                        <li>
+                            <Link
+                                to="/register"
+                                className="btn transparent redborder waves-effect waves-light registerButton"
+                            >
+                                Register
+                            </Link>
+                        </li>
+                        <li>
+                            <Link
+                                to="/login"
+                                className="btn transparent redborder waves-effect waves-light loginButton"
+                            >
+                                Log In
+                            </Link>
+                        </li>
                     </ul>
                 </div>
             </nav>
-        );
+
+            <ul class="sidenav" id="mobile-demo">
+                <li><a href="login">Login</a></li>
+                <li><a href="register">Registration</a></li>
+            </ul>
+        </div>
+    );
 }
 
 export default Navbar;
