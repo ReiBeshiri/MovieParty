f536e9b933d373f67ea4b52a9e2d039ae38abda9
Rei Beshiri <rei.beshiri@studio.unibo.it>
2021-01-25 17:45:32 +0100

    icnon fix

--- movieparty-app/src/components/MovieParty/Chat/InfoBar/InfoBar.js
+++ movieparty-app/src/components/MovieParty/Chat/InfoBar/InfoBar.js
@@ -1,18 +1,14 @@
 import React from 'react';
 import { Link } from "react-router-dom";
-//import onlineIcon from './icons/onlineIcon.png';
+import chatIcon from './icons/chat-icon.png';
 import closeIcon from './icons/go_back_to_dashboard.png';
-
 import './InfoBar.css';
-/**
- * https://p1.hiclipart.com/preview/525/409/291/black-and-colorful-yosemite-style-icons-red-imessage-w-bg-png-clipart.jpg
- * https://i.pinimg.com/originals/cb/f9/b1/cbf9b13467d15911a17db8c00dc3382e.jpg
- */
+
 const InfoBar = ({ room }) => (
   <div className="infoBar">
     <div className="leftInnerContainer">
-      <img className="onlineIcon" src="https://i.pinimg.com/originals/cb/f9/b1/cbf9b13467d15911a17db8c00dc3382e.jpg" alt="chat"/>
-      <h6>{room} room</h6>
+      <img className="onlineIcon" src={chatIcon} alt="chat"/>
+      <h6 class="valign-wrapper">{room} room</h6>
     </div>
     {<div className="rightInnerContainer">
         <Link to="/dashboard">
